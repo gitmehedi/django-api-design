9eb56c755d8a6cee5179bc68076da425bbe50f7c
gitmehedi <git.mehedi@gmail.com>
2023-10-28 09:39:30 +0600

    [ADD] Add files

--- rest_frontend/src/index.js
+++ rest_frontend/src/index.js
@@ -1,4 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import $ from 'jquery';
+import Popper from 'popper.js';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
--- rest_frontend/src/templates/layout/protected/Header.js
+++ rest_frontend/src/templates/layout/protected/Header.js
@@ -2,7 +2,13 @@ import {Link, useNavigate} from "react-router-dom";
 import {logoutUser} from "src/store";
 import {useThunk} from "src/hooks/useThunk";
 import {useSelector} from "react-redux";
-
+import {UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js';
 
 const Header = () => {
     const navigate = useNavigate();
@@ -36,13 +42,22 @@ const Header = () => {
                     </li>
                 </ul>
 
-                <div id="navbarSearch" className="navbar-search w-100 collapse">
-                    <input className="form-control w-100 rounded-0 border-0" type="text" placeholder="Search"
-                           aria-label="Search"/>
-                </div>
-
                 <div>
-                    <Link to='logout/' className="btn btn-primary" onClick={handleLogout}>Logout</Link>
+                    <div className="dropdown" style={{marginRight: `70px`}}>
+                        <a className="dropdown-toggle col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#"
+                           id="navbarDropdownMenuLink"
+                           role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img
+                                src="https://avatars.githubusercontent.com/u/3753206?v=4"
+                                width="40" height="40" className="rounded-circle"/>
+                        </a>
+                        <ul className="">
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <Link to='/profile' className="dropdown-item">Edit Profile</Link>
+                                <Link to='/logout' className="dropdown-item" onClick={handleLogout}>Logout</Link>
+                            </div>
+                        </ul>
+                    </div>
                 </div>
             </header>
         </>
